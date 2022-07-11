634ad2752408932c83f27823ccaa6a294036ea55
Nicolas Aguilar <naguilar@captahydro.com>
2022-07-10 23:21:41 -0300

    compressor.py modified

--- compressor.py
+++ compressor.py
@@ -1,13 +1,15 @@
 from PIL import Image # python3 -m pip install Pillow
 
 import os
+import sys
 
-downloadsFolder = "/Users/nicolasschurmann/Downloads/"
+dir_path = os.getcwd()
 
-if __name__ == "__main__":
-    for filename in os.listdir(downloadsFolder):
-        name, extension = os.path.splitext(downloadsFolder + filename)
+for filename in sys.argv[1:]:
+    file_path = os.path.join(dir_path, filename)
+    name, extension = os.path.splitext(dir_path + '/' + filename)
 
-        if extension in [".jpg", ".jpeg", ".png"]:
-            picture = Image.open(downloadsFolder + filename)
-            picture.save(downloadsFolder + "compressed_"+filename, optimize=True, quality=60)
+    if extension in [".jpg", ".jpeg", ".png"]:
+        picture = Image.open(file_path)
+        path_to_save = os.path.join(dir_path, "compressed_" + filename)
+        picture.save(path_to_save, optimize=True, quality=60)
